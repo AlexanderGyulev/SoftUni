2e3aeecb080a47a39990511cd60fd94491a2dcd1
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2022-06-15 12:18:42 +0300

    Add files via upload
    initial code for the RandomPinGenerator mini project

---
+++ Mini_Projects/RandomPinGenerator/random_pin_generator.py
@@ -0,0 +1,45 @@
+import random
+import sys
+import time
+
+
+how_many_digits = int(input("How many digits do you want your PIN code to have (max 50): "))
+
+# main function, generating a number once the user specifies the desired number of digits
+def main_function():
+    generate_number()
+    # ask the user if they want to generate another PIN
+    another_pin = input("Do you want to generate another PIN (yes/no): ")
+    while another_pin == "yes":
+        generate_number()
+        another_pin = input("Do you want to generate another PIN (yes/no): ")
+    else:
+        print("Thank you for using the program!")
+        time.sleep(5)
+        sys.exit()
+
+
+# generate a random number between 0 and 9 and loop it until the desired number of digits is reached
+def generate_number():
+    checks()
+    total_pin = ""
+    for nums in range(how_many_digits):
+        new_num = random.randint(0, 9)
+        new_num = str(new_num)
+        total_pin = total_pin + new_num
+    print(total_pin)
+
+
+# checking for possible edge cases, for example if the user tries to create a PIN with 0 digits, or if digits > 50
+def checks():
+    global how_many_digits
+    while how_many_digits == 0:
+        print("The number of digits has to be greater than 0!")
+        how_many_digits = int(input("How many digits do you want your PIN code to have (max 50): "))
+    while how_many_digits > 50:
+        print("The number of digits has to be less than 50")
+        how_many_digits = int(input("How many digits do you want your PIN code to have (max 50): "))
+
+
+# calling the main function
+main_function()
