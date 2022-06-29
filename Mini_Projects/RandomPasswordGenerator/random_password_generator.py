a5c9f0fd659140dfe1692bd4a7ff85d79f6c329e
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2022-06-29 17:52:18 +0300

    improved logic
    improved logic and removed the symbols that may be missing from some of the keyboards

--- Mini_Projects/RandomPasswordGenerator/random_password_generator.py
+++ Mini_Projects/RandomPasswordGenerator/random_password_generator.py
@@ -3,35 +3,30 @@ import sys
 import time
 
 password_length = int(input("How long do you want your password to be (max 50 characters): "))
+characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+']
+
 
 # main function, calls the generate_pass() function and asks if the user wants to generate another password
 def main_program():
     generate_pass()
     another_password = input("Do you want to generate another password (yes/no): ")
-    while another_password not in ['yes', 'no']:
-        print("You need to enter yes or no.")
-        another_password = input("Do you want to play another game (yes/no): ")
     while another_password == "yes":
         generate_pass()
         another_password = input("Do you want to generate another password (yes/no): ")
-        while another_password not in ['yes', 'no']:
-            print("You need to enter yes or no.")
-            another_password = input("Do you want to play another game (yes/no): ")
     else:
         print("Thank you for using the program!")
         time.sleep(5)
         sys.exit()
 
 
-# the function that generates the password - it generates a random number in a range and then turns that number to a character
+# the function that generates the password - it generates a random number in a range
+# and then turns that number to a character
 def generate_pass():
     checks()
     final_password = ""
     for _ in range(password_length):
-        characters = random.randint(33, 126)
-        rand = chr(characters)
-        rand = str(rand)
-        final_password += rand
+        random_pw = random.choice(characters)
+        final_password += random_pw
     print(final_password)
 
 
