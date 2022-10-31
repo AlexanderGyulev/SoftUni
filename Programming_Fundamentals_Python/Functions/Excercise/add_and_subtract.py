fc15d9c34c465577550fb1e8e101f5c491a236f0
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2022-10-31 02:47:03 +0200

    Update add_and_subtract.py

--- Programming_Fundamentals_Python/Functions/Excercise/add_and_subtract.py
+++ Programming_Fundamentals_Python/Functions/Excercise/add_and_subtract.py
@@ -3,20 +3,20 @@ second_integer = int(input())
 third_integer = int(input())
 
 
-def sum_numbers():
-    x = first_integer + second_integer
+def sum_numbers(first, second):
+    x = first + second
     return x
 
 
-def subtract():
-    y = sum_numbers() - third_integer
+def subtract(sum, third):
+    y = sum - third
     return y
 
 
-def add_and_subtract():
-    sum_numbers()
-    subtract()
-    return subtract()
+def add_and_subtract(first, second, third):
+    sum_nums = sum_numbers(first, second)
+    subtract_sums = subtract(sum_nums, third)
+    return subtract_sums
 
 
-print(add_and_subtract())
+print(add_and_subtract(first_integer, second_integer, third_integer))
