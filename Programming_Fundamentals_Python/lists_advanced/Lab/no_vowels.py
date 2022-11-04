c408ba698d15f54a229f57fd2215f87bdfc31229
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2022-11-04 19:51:36 +0200

    Update no_vowels.py

--- Programming_Fundamentals_Python/lists_advanced/Lab/no_vowels.py
+++ Programming_Fundamentals_Python/lists_advanced/Lab/no_vowels.py
@@ -1,6 +1,6 @@
 def vowel_removal(text):
-    vowels_list = ["a", "o", "u", "e", "i", "A", "O", "U", "E", "i"]
-    no_vowels = "".join(x for x in text if x not in vowels_list)
+    vowels_list = ["a", "o", "u", "e", "i"]
+    no_vowels = "".join(x for x in text if x.lower() not in vowels_list)
     return no_vowels
 
 
