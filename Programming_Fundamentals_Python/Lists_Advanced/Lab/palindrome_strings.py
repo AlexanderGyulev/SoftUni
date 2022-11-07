d3fc966948e61bb02d4dedf631de970d6efa2cc9
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2022-11-07 02:59:47 +0200

    logic improvement

--- Programming_Fundamentals_Python/Lists_Advanced/Lab/palindrome_strings.py
+++ Programming_Fundamentals_Python/Lists_Advanced/Lab/palindrome_strings.py
@@ -1,17 +1,15 @@
-def palindrome_checker(sequence, word):
+def palindrome_checker(sequence):
     list_of_strings = sequence
     list_of_palindromes = []
     for items in list_of_strings:
         if items == items[:: -1]:
             list_of_palindromes.append(items)
-    palindrome_counter = list_of_strings.count(word)
-    return list_of_palindromes, palindrome_counter
+    return list_of_palindromes
 
 
 sequence_of_strings = input().split(" ")
 palindrome = input()
-result = palindrome_checker(sequence_of_strings, palindrome)
-all_palindromes = result[0]
-counter = result[1]
-print(all_palindromes)
+result = palindrome_checker(sequence_of_strings)
+counter = sequence_of_strings.count(palindrome)
+print(result)
 print(f"Found palindrome {counter} times")
