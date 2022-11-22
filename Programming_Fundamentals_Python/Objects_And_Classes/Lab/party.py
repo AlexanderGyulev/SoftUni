aad22fec01dff1229804cf0f145bc073be382c83
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2022-11-22 02:45:55 +0200

    Update party.py

--- Programming_Fundamentals_Python/Objects_And_Classes/Lab/party.py
+++ Programming_Fundamentals_Python/Objects_And_Classes/Lab/party.py
@@ -2,12 +2,21 @@ class Party():
     def __init__(self):
         self.people = []
 
+    def invite(self, person):
+        self.people.append(person)
+
+    def total_names(self):
+        return f'{", ".join(party.people)}'
+
+    def number_of_people(self):
+        return f'{len(party.people)}'
+
 
 party = Party()
 names = input()
 while names != "End":
-    party.people.append(names)
+    party.invite(names)
     names = input()
 
-print(f'Going: {", ".join(party.people)}')
-print(f'Total: {len(party.people)}')
+print(f'Going: {party.total_names()}')
+print(f'Total: {party.number_of_people()}')
