c59c26590ed8c3a569623ccb0653c43c75ba0450
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2022-11-25 19:36:18 +0200

    code improvement

--- Programming_Fundamentals_Python/Dictionaries/Exercise/phonebook.py
+++ Programming_Fundamentals_Python/Dictionaries/Exercise/phonebook.py
@@ -2,16 +2,17 @@ phonebook_dictionary = {}
 while True:
     command = input()
     if command.isdigit():
-        for numbers in range(int(command)):
-            name_to_search = input()
-            if name_to_search in phonebook_dictionary:
-                phone_num = phonebook_dictionary[name_to_search]
-                print(f"{name_to_search} -> {phone_num}")
-            else:
-                print(f"Contact {name_to_search} does not exist.")
         break
     else:
         command = command.split("-")
         name = command[0]
         phone = command[1]
         phonebook_dictionary[name] = phone
+
+for numbers in range(int(command)):
+    name_to_search = input()
+    if name_to_search in phonebook_dictionary:
+        phone_num = phonebook_dictionary[name_to_search]
+        print(f"{name_to_search} -> {phone_num}")
+    else:
+        print(f"Contact {name_to_search} does not exist.")
