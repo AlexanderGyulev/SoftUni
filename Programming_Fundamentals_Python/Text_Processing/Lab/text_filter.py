7b897c02d99d03faeca4908b062203c010980a46
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2022-12-02 12:01:49 +0200

    logic improvement

--- Programming_Fundamentals_Python/Text_Processing/Lab/text_filter.py
+++ Programming_Fundamentals_Python/Text_Processing/Lab/text_filter.py
@@ -2,6 +2,5 @@ banned_words = input().split(", ")
 text = input()
 
 for words in banned_words:
-    while words in text:
-        text = text.replace(words, "*" * len(words))
+    text = text.replace(words, "*" * len(words))
 print(text)
