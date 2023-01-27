de3c1948e1f49d5497865f7121e853e3c1f85e5f
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2023-01-27 18:01:58 +0200

    Update fashion_boutique.py

--- Python_Advanced/Lists_as_Stacks_and_Queues/Exercise/fashion_boutique.py
+++ Python_Advanced/Lists_as_Stacks_and_Queues/Exercise/fashion_boutique.py
@@ -3,8 +3,6 @@ from collections import deque
 sequence_of_clothes = deque(int(x) for x in input().split(" "))
 rack_capacity = int(input())
 
-sequence_of_clothes.reverse()
-
 total_racks = 1
 
 while len(sequence_of_clothes) > 0:
@@ -12,11 +10,11 @@ while len(sequence_of_clothes) > 0:
     for items in sequence_of_clothes.copy():
         if current_rack_total + items <= rack_capacity:
             current_rack_total += items
-            sequence_of_clothes.popleft()
+            sequence_of_clothes.pop()
         else:
             total_racks += 1
             current_rack_total = 0
             current_rack_total += items
-            sequence_of_clothes.popleft()
+            sequence_of_clothes.pop()
 
 print(total_racks)
