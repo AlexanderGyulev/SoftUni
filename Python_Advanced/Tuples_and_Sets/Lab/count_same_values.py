f447965e3dba667ac6d1ca3fd2ee2457307451b1
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2023-03-10 00:06:12 +0200

    Update count_same_values.py

--- Python_Advanced/Tuples_and_Sets/Lab/count_same_values.py
+++ Python_Advanced/Tuples_and_Sets/Lab/count_same_values.py
@@ -4,9 +4,7 @@ nums_dict = {}
 
 for elements in nums:
     if elements not in nums_dict.keys():
-        nums_dict[elements] = 1
-    else:
-        nums_dict[elements] += 1
+        nums_dict[elements] = nums.count(elements)
 
 for keys in nums_dict.keys():
     print(f"{keys} - {nums_dict[keys]} times")
