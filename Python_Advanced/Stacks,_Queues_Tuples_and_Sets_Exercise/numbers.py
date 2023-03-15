23ef7be2bb52890a74625de07592c36385b41d97
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2023-03-15 21:10:21 +0200

    Update numbers.py

--- Python_Advanced/Stacks,_Queues_Tuples_and_Sets_Exercise/numbers.py
+++ Python_Advanced/Stacks,_Queues_Tuples_and_Sets_Exercise/numbers.py
@@ -1,5 +1,5 @@
-first_sequence_of_integers = set(map(int, input().split()))
-second_sequence_of_integers = set(map(int, input().split()))
+first = set(map(int, input().split()))
+second = set(map(int, input().split()))
 
 lines_of_input = int(input())
 
@@ -9,35 +9,32 @@ for _ in range(lines_of_input):
         split_command = current_command.split(" ")
         for element in split_command:
             if element.isdigit():
-                first_sequence_of_integers.add(int(element))
+                first.add(int(element))
     elif "Add Second" in current_command:
         split_command = current_command.split(" ")
         for element in split_command:
             if element.isdigit():
-                second_sequence_of_integers.add(int(element))
+                second.add(int(element))
     elif "Remove First" in current_command:
         split_command = current_command.split(" ")
         for element in split_command:
             if element.isdigit():
                 element = int(element)
-                first_sequence_of_integers.discard(element)
+                first.discard(element)
     elif "Remove Second" in current_command:
         split_command = current_command.split(" ")
         for element in split_command:
             if element.isdigit():
                 element = int(element)
-                second_sequence_of_integers.discard(element)
+                second.discard(element)
     elif "Check Subset" in current_command:
-        if first_sequence_of_integers.issubset(second_sequence_of_integers):
-            print(True)
-        elif second_sequence_of_integers.issubset(first_sequence_of_integers):
+        if first.issubset(second) or second.issubset(first):
             print(True)
         else:
             print(False)
 
-first_sequence_of_integers = sorted(first_sequence_of_integers)
-second_sequence_of_integers = sorted(second_sequence_of_integers)
+first = sorted(first)
+second = sorted(second)
 
-print(*first_sequence_of_integers, sep=", ")
-print(*second_sequence_of_integers,sep=", ")
-
+print(*first, sep=", ")
+print(*second, sep=", ")
